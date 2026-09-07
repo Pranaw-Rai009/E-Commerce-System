5b7b015305a8f117889fb2ee658cde085fccc726
Pranaw Rai <dhanrajrai2351@gmail.com>
2026-09-07 13:25:16 +0545

    feat: access token verification

--- src/middlewares/authAccess.middleare.js
+++ src/middlewares/authAccess.middleare.js
@@ -1,7 +1,15 @@
 import { asyncHandler } from '../utils/asyncHandler.utils.js'
 import ApiError from '../utils/apiError.utils.js'
+import jwt from 'jsonwebtoken'
 
 
-export const authAccess = asyncHandler(async (req, res) => {
+export const authAccess = asyncHandler(async (req, res, next) => {
+    const incomingAccesToken = req.header["authorization"]
+    const accessToken = incomingAccesToken.split(" ")[1]
+
+    const isVerified = jwt.verify(accessToken, process.env.ACCESS_TOKEN_SECRET)
+    if(!isVerified) throw new ApiError(401, "Invalid Access Token")
     
+    req.user = isVerified
+    next()
 })
