0eb23441b150b59232c5fffb3a5686211d5d10fc
Pranaw Rai <dhanrajrai2351@gmail.com>
2026-09-07 15:22:49 +0545

    fix: Added refreshtoken table in model

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,12 +1,15 @@
 import { Router } from 'express'
-import { userRegister, getAllSeller, getAllCustomer } from '../controllers/user.controller.js'
+import { userRegister, getAllSeller, getAllCustomer, updateUser, updatePassword } from '../controllers/user.controller.js'
 import { loginUser } from '../controllers/login.controller.js'
 import { refreshAccessToken } from '../controllers/refreshAccessToken.controller.js'
+import { authAccess } from '../middlewares/authAccess.middleare.js'
 const router = Router()
 
 router.post("/register", userRegister)
 router.post("/login", loginUser)
 router.post("/refreshAccessToken", refreshAccessToken)
+router.post("/update", authAccess, updateUser)
+router.post("/updatePassword", authAccess, updatePassword)
 router.get("/allSellers/:role", getAllSeller)
 router.get("/allCustomers/:role", getAllCustomer)
 
