efb04bd5278c2842c14629e83d81e12bf0593da9
Pranaw Rai <dhanrajrai2351@gmail.com>
2026-09-10 15:23:24 +0545

    feat: Added feat to chekc if the item already exist in cart

--- src/controllers/cartItem.controller.js
+++ src/controllers/cartItem.controller.js
@@ -2,12 +2,34 @@ import { asyncHandler } from "../utils/asyncHandler.utils.js";
 import ApiError from "../utils/apiError.utils.js";
 import { prisma } from "../db/dbConnect.js";
 
-export const createCartItem = async(productQuantity, productId, myCartId) => {
-    return await prisma.cartItems.create({
-        data: {
+export const createCartItem = async (productQuantity, productId, myCartId) => {
+    // Checking if the item already exists in my cart
+    const itemAlreadyExist = await prisma.cartItems.findFirst({
+        where: {
             productId,
-            quantity: productQuantity,
             cartId: myCartId
         }
     })
+    if (itemAlreadyExist) {
+        return await prisma.cartItems.update({
+            where:{
+                id: itemAlreadyExist.id
+            },
+            data: {
+                productId,
+                quantity: (productQuantity + itemAlreadyExist.quantity),
+                cartId: myCartId
+            }
+        })
+    } else {
+        return await prisma.cartItems.create({
+            data: {
+                productId,
+                quantity: productQuantity,
+                cartId: myCartId
+            }
+        })
+
+    }
 }
+
