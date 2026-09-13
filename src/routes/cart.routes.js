f943a03083663a2cd1bb85103a73c4e288155ee2
Pranaw Rai <dhanrajrai2351@gmail.com>
2026-09-13 10:22:30 +0545

    feat: add cart management routes

--- src/routes/cart.routes.js
+++ src/routes/cart.routes.js
@@ -1,8 +1,11 @@
 import { Router } from "express";
-import { openMyCart } from '../controllers/cart.controller.js'
+import { openMyCart, addToCart, removeItemsFromCart, getMyCart } from '../controllers/cart.controller.js'
 import { authAccess } from "../middlewares/authAccess.middleware.js";
 const router = Router()
 
 router.get("/myCart", authAccess, openMyCart)
+router.post("/addToCart/:productId", authAccess, addToCart)
+router.delete("/remove", authAccess, removeItemsFromCart)
+router.get("/getMyCart", authAccess, getMyCart)
 
 export default router;
