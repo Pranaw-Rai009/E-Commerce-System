cb33d21b3903e57d0b62f5fb5e6d32cb6f675117
Pranaw Rai <dhanrajrai2351@gmail.com>
2026-09-14 13:37:40 +0545

    feat: upgraded the logic of ordering items

--- src/controllers/order.controller.js
+++ src/controllers/order.controller.js
@@ -4,22 +4,58 @@ import { prisma } from "../db/dbConnect.js";
 import createOrderItem from "../controllers/cartItem.controller.js"
 
 export const createNewOrder = asyncHandler(async (req, res) => {
-    
+
     const cartItemsIds = req.body  //receiving the cartItems inthe form or array
     if (cartItemsIds.length === 0 || !cartItemsIds) throw new ApiError(400, "No items selected")
 
-    const cart = await prisma.cart.findUnique({
+    const userData = await prisma.user.findFirst({
         where: {
-            userId: req.user.id
+            io: req.user.id
         }
     })
 
-    const newOrderItems = await createNewOrder(cartItemsIds, cart.id, )
-    const createOrder = await prisma.order.create({
+    const orderDetailsPromises = cartItemsIds.map(async (itemId) => {
+        return await prisma.cartItems.findFirst({
+            where: {
+                id: itemId,
+                cart: {
+                    userId: req.user.id
+                }
+            },
+            include: {
+                product: {
+                    select: {
+                        price: true
+                    }
+                }
+            }
+        })
+    })
+    const orderDetail = await Promise.all(orderDetailsPromises)
+
+    const forEachOrder = orderDetails.map(async (order1) => {
+        const subTotalPrice = order1.quantity * order1.product.price
+        const deliveryCharge = subTotalPrice >= 10000 ? 0 : 130
+        return { subTotalPrice, deliveryCharge }
+    })
+
+    let subTotal = 0
+    let deliveryCharge = 0
+    for (const item of forEachOrder) {
+        subTotal += item.subTotalPrice
+        deliveryCharge += item.deliveryCharge
+    }
+    const totalAmount = subtotal + deliveryCharge
+
+    const newOrder = await prisma.order.create({
         data: {
             userId: req.user.id,
-            deliveryCharge
+            deliveryCharge,
+            subTotal,
+            totalAmount,
+            shippingAddress: userData.shippingAddress,
         }
     })
-
+    if(!newOrder) throw new ApiError(500, "Error occured while creating product!")
+    res.status(201).json(newOrder)
 })
