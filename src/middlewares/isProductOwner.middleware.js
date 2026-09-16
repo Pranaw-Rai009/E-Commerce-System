12604144ab594b68e2c65ee54e22be64077b0441
Pranaw Rai <dhanrajrai2351@gmail.com>
2026-09-16 15:13:48 +0545

    feat: add is product ownership check

---
+++ src/middlewares/isProductOwner.middleware.js
@@ -0,0 +1,19 @@
+import { asyncHandler } from "../utils/asyncHandler.utils.js";
+import ApiError from "../utils/apiError.utils.js";
+import { prisma } from "../db/dbConnect.js";
+
+export const isProductOwner = asyncHandler(async (req, res, next) => {
+    const prodId = req.params.productId
+    if (!prodId) throw new ApiError(400, "Product Id is missing!")
+    const userId = req.user.id
+
+    const isOwner = await prisma.product.findfirst({
+        where: {
+            id: prodId,
+            sellerId: userId
+        }
+    })
+    if (!isOwner) throw new ApiError(401, "Access Denied [You are not the owner]")
+    req.isOwner = isOwner
+    next()
+})
