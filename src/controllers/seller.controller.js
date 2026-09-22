f91d41fe2abdd0409c9eff42421fa8b75967301f
Pranaw Rai <dhanrajrai2351@gmail.com>
2026-09-22 13:31:00 +0545

    feat: add update order status functionality by user

--- src/controllers/seller.controller.js
+++ src/controllers/seller.controller.js
@@ -1,7 +1,8 @@
 import { asyncHandler } from "../utils/asyncHandler.utils.js";
 import ApiError from "../utils/apiError.utils.js";
+import { prisma } from '../db/dbConnect.js'
 
-export const getMeAllMyOrders = asyncHandler(async(requ, res) => {
+export const getMeAllMyOrders = asyncHandler(async (req, res) => {
     const userId = req.user.userId
     const newOrders = await prisma.orderItems.findMany({
         where: {
@@ -26,7 +27,40 @@ export const getMeAllMyOrders = asyncHandler(async(requ, res) => {
             }
         }
     })
-    if(!newOrders) throw new ApiError(500, "Error occured while fetching my new orders!")
-    res.status(200).json({message: "My orders", orders: newOrders})
-
+    if (!newOrders) throw new ApiError(500, "Error occured while fetching my new orders!")
+    res.status(200).json({ message: "My orders", orders: newOrders })
 })
+
+export const updateOrderStatus = asyncHandler(async(req, res) => {
+    const orderId = req.params.orderId
+    if(!orderId) throw new ApiError(400, "Order Id is missing!")
+    
+    const { newStatus } = req.body
+    if(!newStatus) throw new ApiError(401, "New Status is required!")
+    
+    const isMyOrder = await prisma.order.findMany({
+        where: {
+            id: orderId,
+            orderItems: {
+                product: {
+                    sellerId: req.user.id
+                }
+            }
+        }
+    })
+    if(!isMyOrder || isMyOrder.length === 0) throw new ApiError(404, "Order not found!")
+    
+    const updatingOrder = await prisma.order.update({
+        where: {
+            id: orderId
+        },
+        data: {
+            status: newStatus
+        }
+    })
+    if(!updatingOrder) throw new ApiError(500, "Error occured while updating the order")
+    
+    res.status(200).json({message: "Order Status Updated", updatingOrder})
+})
+
+// export const 
