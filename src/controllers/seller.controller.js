8a33744a2b0f2eba1417612955713be9d29f3a79
Pranaw Rai <dhanrajrai2351@gmail.com>
2026-09-22 08:43:04 +0545

    feat: added get my new orders functionality

---
+++ src/controllers/seller.controller.js
@@ -0,0 +1,32 @@
+import { asyncHandler } from "../utils/asyncHandler.utils.js";
+import ApiError from "../utils/apiError.utils.js";
+
+export const getMeAllOrders = asyncHandler(async(requ, res) => {
+    const userId = req.user.userId
+    const newOrders = await prisma.orderItems.findMany({
+        where: {
+            product: {
+                sellerId: userId
+            }
+        },
+        include: {
+            product: {
+                select: {
+                    id: true,
+                    title: true,
+                }
+            },
+            order: {
+                select: {
+                    id: true,
+                    userId: true,
+                    createdAt: true,
+                    status: true
+                }
+            }
+        }
+    })
+    if(!newOrders) throw new ApiError(500, "Error occured while fetching my new orders!")
+    res.status(200).json({message: "My orders", orders: newOrders})
+
+})
